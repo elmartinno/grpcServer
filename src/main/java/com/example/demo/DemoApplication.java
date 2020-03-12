f645f9aab500bb3c3ff8b262bc1c0d083d4e8513
Martin Komorny <martinko.komorny@gmail.com>
2020-03-12 21:27:46 +0100

    tak

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,12 @@
 package com.example.demo;
 
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.grpc.Server;
-import io.grpc.ServerBuilder;
-import io.grpc.protobuf.services.ProtoReflectionService;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 
-@SpringBootApplication
+//@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
