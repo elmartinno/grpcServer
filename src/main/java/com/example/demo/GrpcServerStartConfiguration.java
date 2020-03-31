ec7a8d11904686134bf6c468841b431d6b41482d
Martin Komorny <martinko.komorny@gmail.com>
2020-03-31 23:06:51 +0200

    d

--- src/main/java/com/example/demo/GrpcServerStartConfiguration.java
+++ src/main/java/com/example/demo/GrpcServerStartConfiguration.java
@@ -18,9 +18,9 @@ public class GrpcServerStartConfiguration {
 	//@PostConstruct
 	public void startGrpcServer() throws IOException, InterruptedException {
 		Server server = ServerBuilder.forPort(31111)
-//				.useTransportSecurity(
-//						TlsTesting.loadCert("server1.pem"),
-//						TlsTesting.loadCert("server1.key"))
+				.useTransportSecurity(
+						TlsTesting.loadCert("server1.pem"),
+						TlsTesting.loadCert("server1.key"))
 				.addService(new AccountController())
 				.addService(new TurnoverController())
 //				.addService(ProtoReflectionService.newInstance()) // added Reflection
